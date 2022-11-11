38c2254417f256bf68f39a406da73a5c544ef303
Dongkeun <hongdongk@naver.com>
2022-11-11 19:26:48 +0900

    Create targetnumber_Pro.py

---
+++ DFS,BFS/targetnumber_Pro.py
@@ -0,0 +1,34 @@
+# n개의 음이 아닌 정수들이 있습니다. 
+# 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 
+# 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+# -1+1+1+1+1 = 3
+# +1-1+1+1+1 = 3
+# +1+1-1+1+1 = 3
+# +1+1+1-1+1 = 3
+# +1+1+1+1-1 = 3
+
+# 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 
+# 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+# BFS 풀이
+from collections import deque
+
+def solution(numbers, target):
+    
+    queue = deque()
+    answer = 0
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    
+    while queue:
+        [temp , idx] = queue.popleft()
+        idx+=1 
+        if idx < len(numbers):    
+            queue.append([temp + numbers[idx],idx])
+            queue.append([temp - numbers[idx],idx])
+        else:
+            if temp == target:
+                answer+=1
+    
+    return answer
