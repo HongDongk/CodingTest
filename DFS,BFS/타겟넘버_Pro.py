f0415b987f4536bc2fe23f38a6f7121d155398b6
Dongkeun <hongdongk@naver.com>
2022-11-19 23:00:16 +0900

    타겟넘버 dfs풀이추가

--- DFS,BFS/타겟넘버_Pro.py
+++ DFS,BFS/타겟넘버_Pro.py
@@ -11,6 +11,21 @@
 # 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 
 # 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
 
+# DFS 풀이
+answer = 0
+def solution(numbers, target):
+    
+    def dfs(idx, result):
+        global answer
+        if idx == len(numbers):
+            if result == target:
+                answer +=1
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])   
+    dfs(0,0)
+    return answer
+
 # BFS 풀이
 from collections import deque
 
