049f1824e7a364342629231bbe9e60b75d7eefc4
Dongkeun <hongdongk@naver.com>
2022-11-24 19:58:45 +0900

    Create 전화번호목록_Pro.py

---
+++ 해쉬/전화번호목록_Pro.py
@@ -0,0 +1,23 @@
+# 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+# 전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+# 구조대 : 119
+# 박준영 : 97 674 223
+# 지영석 : 11 9552 4421
+
+# 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 
+# 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
+def solution(phone_book):
+    hash = {}
+    for i in phone_book:
+        hash[i] = "폰넘버"
+    
+    for i in phone_book:
+        jopdu = ''
+        for j in i:
+            jopdu+=j
+            if jopdu in hash and jopdu != i :
+                return False
+    
+    return True
