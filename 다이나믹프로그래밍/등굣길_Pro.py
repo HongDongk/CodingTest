30194df21bd2b251ef902fa1cbf0b011a5cc5043
Dongkeun <hongdongk@naver.com>
2022-12-20 12:33:07 +0900

    Update 등굣길_Pro.py

--- 다이나믹프로그래밍/등굣길_Pro.py
+++ 다이나믹프로그래밍/등굣길_Pro.py
@@ -4,3 +4,20 @@
 # 가장 왼쪽 위, 즉 집이 있는 곳의 좌표는 (1, 1)로 나타내고 가장 오른쪽 아래, 즉 학교가 있는 곳의 좌표는 (m, n)으로 나타냅니다.
 # 격자의 크기 m, n과 물이 잠긴 지역의 좌표를 담은 2차원 배열 puddles이 매개변수로 주어집니다. 
 # 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수를 1,000,000,007로 나눈 나머지를 return 하도록 solution 함수를 작성해주세요.
+
+
+def solution(m, n, puddles):
+    dp = [[0 for i in range(m+1)] for j in range(n+1)]
+    dp[1][1] = 1
+    puddles = [[q,p] for [p,q] in puddles]
+    
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if i==1 and j==1 :
+                continue
+            if [i,j] in puddles:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1000000007
+    
+    return dp[n][m]
