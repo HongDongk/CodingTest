4d82c8dadab331093590f8b9c1826b19e893b6f6
Dongkeun <hongdongk@naver.com>
2022-12-22 22:51:10 +0900

    Update 10828.py

--- 스택&큐/백준/10828.py
+++ 스택&큐/백준/10828.py
@@ -27,14 +27,14 @@ for i in range(N):
             print(stack.pop())
     elif order == "size":
         print(len(stack))
-    elif order =="top":
-        if len(stack)==0:
-            print(-1)
-        else:
-            print(stack[-1])
     elif order == "empty":
         if len(stack) == 0:
             print(1)
         else:
             print(0)
+    elif order =="top":
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
      
