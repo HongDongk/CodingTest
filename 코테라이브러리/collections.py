541998f0b813001e4e1c317ed211a00639254783
Dongkeun <hongdongk@naver.com>
2022-12-28 14:17:21 +0900

    Update collections.py

--- 코테라이브러리/collections.py
+++ 코테라이브러리/collections.py
@@ -14,10 +14,8 @@ from collections import Counter
 
 answer = Counter([1, 1, 2, 3, 3])
 
-# Counter({1: 2, 3 : 2, 2 : 1})  --> answer 출력시 값
+# Counter({1: 2, 3 : 2, 2 : 1})  --> answer 출력시 값 (값이 큰순서대로 정렬)
 
-answer[1] # 배열안에서 1이 몇개인지 파악 - 2
-answer[2] # 배열안에서 2가 몇개인지 파악 - 1
 
 
 
