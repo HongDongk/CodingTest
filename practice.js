f352baaba25543d5efa7833bda5cbd25a70ed3e8
Dongkeun <hongdongk@naver.com>
2023-01-23 23:26:16 +0900

    최빈값구하기

--- practice.js
+++ practice.js
@@ -1,1 +1,6 @@
-
+array = [1,2,3,3,3,4]
+let m = new Map();
+for (let n of array) m.set(n, (m.get(n) || 0)+1);
+m = [...m];
+m.sort((a,b)=> b[1]-a[1]);
+console.log(m[0][0]);
---
+++ 입문100제/최빈값구학기.js
@@ -0,0 +1,10 @@
+// Map 사용하기(객체를 팬시하게만들어줌) - set으로 키, 값입력 get으로 값 불러오기
+// [...m]으로 Map객체 배열로 바꿀 수 잇음
+
+function solution(array) {
+    let m = new Map();
+    for (let n of array) m.set(n, (m.get(n) || 0)+1);
+    m = [...m].sort((a,b)=>b[1]-a[1]);
+    if (m.length === 1 || m[0][1] > m[1][1]) return m[0][0]
+    else return -1
+}
