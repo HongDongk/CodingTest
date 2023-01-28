ed9e5fbf8517a61d2c0cfc579051b8fed0cb16c0
Dongkeun <hongdongk@naver.com>
2023-01-28 14:58:22 +0900

    문자열반복출력하기

---
+++ 입문100제/문자반복출력하기.js
@@ -0,0 +1,7 @@
+// 문자열을 배열로바꿀떼 [...str]
+// 문자열반복하고싶을때 str.repeat(n) (str을 n번 만큼반복)
+
+function solution(my_string, n) {
+    a = [...my_string].map((a) => a.repeat(n)).join('')
+    return a;
+}
--- 입문100제/배열뒤집기.js
+++
@@ -1,6 +0,0 @@
-// 배열을 뒤집을땐 reverse()
-
-function solution(num_list) {
-    num_list.reverse()
-    return num_list;
-}
