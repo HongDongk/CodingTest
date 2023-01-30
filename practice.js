10b4ad6b96e14392d8feecbbc71435d463765dd7
Dongkeun <hongdongk@naver.com>
2023-01-30 23:18:06 +0900

    가위바위보

--- practice.js
+++ practice.js
@@ -1,1 +1,7 @@
+winning = {
+    0 : '5',
+    1 : '0',
+    a : '2',
+}
 
+console.log(winning.0);
--- 입문100제/가위바위보.js
+++ 입문100제/가위바위보.js
@@ -1,5 +1,6 @@
 // 유사배열
-// 밑에 winning은 배열처럼 보이는 객체이다!! 유사배열 따라서 winning[a]로 불러야 값을 얻을수있다!
+// 밑에 winning은 배열처럼 보이는 객체이다!! 따라서 length를 쓸수 없고, key값이 타입이 있으므로 winning[]으로 불러와야 한다.
+
 
 function solution(rsp) {
     winning = {
