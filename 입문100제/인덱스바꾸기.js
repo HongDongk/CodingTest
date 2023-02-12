531189b42801818320b8f11b6d0fdad1ad5fbccf
Dongkeun <hongdongk@naver.com>
2023-02-12 21:36:19 +0900

    Create 인덱스바꾸기.js

---
+++ 입문100제/인덱스바꾸기.js
@@ -0,0 +1,9 @@
+// 구조분해할당!!!!!!!!!
+
+function solution(my_string, num1, num2) {
+    my_string = my_string.split('');
+    
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    
+    return my_string.join('');
+}
