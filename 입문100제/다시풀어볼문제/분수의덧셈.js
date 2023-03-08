3ca194feb51f6dea6d68f9eae19fd0c6bcce6ca1
Dongkeun <hongdongk@naver.com>
2023-03-08 17:12:13 +0900

    Update 분수의덧셈.js

--- 입문100제/다시풀어볼문제/분수의덧셈.js
+++ 입문100제/다시풀어볼문제/분수의덧셈.js
@@ -1,5 +1,6 @@
 // 최대공약수 구하는방법
 
+// 재귀
 function GCD(a,b){
     return(a%b? GCD(b,a%b):b)
 }
@@ -10,4 +11,20 @@ function solution(numer1, denom1, numer2, denom2) {
     let gcd = GCD(bunmo,bunza)
     
     return [bunza/gcd, bunmo/gcd];
+}
+
+// for문
+function solution(numer1, denom1, numer2, denom2) {
+    let i = 1;
+    const a = denom1*denom2; // 분모
+    const b = numer1*denom2 + numer2*denom1;  // 분자
+    
+    for (let x = 2; x<=Math.min(a,b); x++){
+        if (a%x === 0 && b%x === 0 ){
+            i = x;
+        }
+    }
+    
+    return [b/i, a/i]
+    
 }
