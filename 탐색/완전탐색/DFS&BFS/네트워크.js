24967fb764220d78401f9e3976a19ef36a100a31
Dongkeun <hongdongk@naver.com>
2023-03-11 17:11:43 +0900

    Update 네트워크.js

--- 탐색/완전탐색/DFS&BFS/네트워크.js
+++ 탐색/완전탐색/DFS&BFS/네트워크.js
@@ -3,21 +3,22 @@
 function solution(n, computers) {
     let visited = new Array(n).fill(false);
     let answer = 0;
-
+    
     function dfs(i) {
         visited[i] = true;
-        for(let j=0; j<computers[i].length; j++) {
-            if(computers[i][j]===1 && !visited[j]){
+        for (let j=0; j<computers[i].length; j++){
+            if(visited[j] === false && computers[i][j] ===1 ){
                 dfs(j);
             }
         }
     }
     
-    for (let i=0; i < computers.length; i++) {
-        if (!visited[i]) {
-            dfs(i)
-            answer++;
+    for (let i in computers){
+        if(visited[i] === false){
+            dfs(i);
+            answer+=1;
         }
     }
+    
     return answer;
 }
