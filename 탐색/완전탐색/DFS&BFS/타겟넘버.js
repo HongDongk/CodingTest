906929eff76f0904d8a1704b4994a0d51de76d40
Dongkeun <hongdongk@naver.com>
2023-03-11 15:45:51 +0900

    Update 타겟넘버.js

---
+++ 탐색/완전탐색/DFS&BFS/타겟넘버.js
@@ -0,0 +1,20 @@
+// 조합 - DFS(재귀함수)
+
+function solution(numbers, target) {
+    let answer = 0;
+    
+    function dfs(count, value){
+        if(count<numbers.length){
+            dfs(count+1, value+numbers[count]);
+            dfs(count+1, value-numbers[count]);
+        } else{
+            if(value === target){
+                answer+=1;
+            }
+        } 
+    }
+    
+    dfs(0, 0);
+    
+    return answer;
+}
