38b605e19573cdd72ca92677e24c8749ec41b891
Dongkeun <hongdongk@naver.com>
2023-03-15 13:07:54 +0900

    카펫

--- practice.js
+++ practice.js
@@ -1,17 +1,20 @@
 function getPermutations(arr, selectNumber) {
     const results = [];
-    if (selectNumber === 1) return arr.map((a) => [a]); 
+    
+    if(selectNumber === 1){
+        return arr.map((a) => [a]);
+    }
 
-    arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index+1)] //fixed값 빼고 만들어진배열
-      const permutations = getPermutations(rest, selectNumber - 1); // rest배열에서 하나작게 뽑기
-      const attached = permutations.map((a) => [fixed, ...a]); // fixed값과 재귀함수돌린값로 만들어진배열
-      results.push(...attached); 
-    });
+    arr.forEach((fixed, index, array) => {
+        const rest = [...array.slice(0,index) , ...array.slice(index+1)];
+        const permutation = getPermutations(rest, selectNumber-1);
+        const attached = permutation.map((a) => [...fixed, a]);
+        results.push(...attached);
+    })
 
     return results; 
 }
 
 
 
-console.log(getPermutations(['a','b','c'] , 3));
+console.log(getPermutations(['a','b','c','d'] , 2));
