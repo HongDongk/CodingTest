ff72f325fc7d5e78ae1ecb567191d71beb32f95d
Dongkeun <hongdongk@naver.com>
2023-03-20 18:27:48 +0900

    Create H-Index.js

---
+++ 탐색/정렬/H-Index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+    citations = citations.sort((a, b) => b - a);
+    let i = 0;
+    
+    while (i + 1 <= citations[i]) {
+        i++;
+    }
+    
+    return i;
+
+}
