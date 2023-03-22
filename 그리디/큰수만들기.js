e90bce6fc23d39603392861b1e5eb943b4408551
Dongkeun <hongdongk@naver.com>
2023-03-22 12:05:59 +0900

    Update 큰수만들기.js

--- 그리디/큰수만들기.js
+++ 그리디/큰수만들기.js
@@ -1,16 +1,16 @@
-// splice - 배열을 자르고싶을때 array.splice(시작인덱스, 갯수);
+// splice - 배열을 제거하고싶을때 array.splice(제거할인덱스, 제거할인덱스부터 삭제할 갯수);
 
 function solution(number, k) {
-    const arr = [];
-    for (let i = 0; i < number.length; i++) {
-      while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
-        k--;
-        arr.pop();
+  const arr = [];
+  for(let i =0; i < number.length; i++){
+      while(arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0){
+          k--;
+          arr.pop();
       }
       arr.push(number[i]);
-    }
-      
-    arr.splice(number.length - k, k); 
-    
-    return arr.join("");
+  }
+  
+  arr.splice(arr.length-1,k)
+  
+  return arr.join('');
 }
