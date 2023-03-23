2ee6e24729538027028efe789d8e0d3c1c7e2286
Dongkeun <hongdongk@naver.com>
2023-03-23 14:19:15 +0900

    Create 단속카메라.js

---
+++ 그리디/단속카메라.js
@@ -0,0 +1,20 @@
+// 조건처리하기
+
+function solution(routes) {
+    routes.sort((a,b) => a[0] - b[0]);
+    let out = routes[0][1];
+    let answer = 1;
+    
+    for(let i =1; i<routes.length; i++){
+        if(out<routes[i][0]){
+            answer++;
+            out = routes[i][1];
+        } 
+        
+        if(out>routes[i][1]) {
+            out = routes[i][1];
+        }
+    }
+    
+    return answer;
+}
