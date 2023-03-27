e2a6f51fc9fd356c17af5f61288f44c76384a95a
Dongkeun <hongdongk@naver.com>
2023-03-27 18:03:46 +0900

    Create 정수제곱근판별.js

---
+++ _Level1/정수제곱근판별.js
@@ -0,0 +1,10 @@
+// 정수인지아닌지 판별 - Number.isInteger()
+// 제곱근 - Math.sqrt(n)
+
+function solution(n) {
+    if(Number.isInteger(Math.sqrt(n))){
+       return (Math.sqrt(n)+1) ** 2;
+    } else{
+        return -1;
+    }
+}
