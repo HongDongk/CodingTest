364ff13a956165089b2a16ac94106159903c392e
Dongkeun <hongdongk@naver.com>
2023-04-10 16:05:09 +0900

    Create 숫자짝꿍.js

---
+++ _Level1/숫자짝꿍.js
@@ -0,0 +1,19 @@
+function solution(X, Y) {
+  let answer = "";
+  X = X.split("");
+  Y = Y.split("");
+
+  for (let i = 0; i < 10; i++) {
+    const curX = X.filter((a) => parseInt(a) === i).length;
+    const curY = Y.filter((a) => parseInt(a) === i).length;
+    answer += String(i).repeat(Math.min(curX, curY));
+  }
+
+  if (answer === "") return "-1";
+  if (parseInt(answer) === 0) return "0";
+
+  return answer
+    .split("")
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .join("");
+}
