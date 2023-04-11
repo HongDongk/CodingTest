187a258a24629cdf77ad800f502f84b2a8df7947
Dongkeun <hongdongk@naver.com>
2023-04-11 18:39:24 +0900

    신규아이디추천

---
+++ 정규식/(카카오)신규아이디추천.js
@@ -0,0 +1,16 @@
+// 어려운정규식!!
+
+function solution(new_id) {
+  const answer = new_id
+    .toLowerCase() // 1
+    .replace(/[^\w-_.]/g, "") // 2
+    .replace(/\.+/g, ".") // 3
+    .replace(/^\.|\.$/g, "") // 4
+    .replace(/^$/, "a") // 5
+    .slice(0, 15)
+    .replace(/\.$/, ""); // 6
+
+  const len = answer.length;
+
+  return len > 2 ? answer : answer + answer[len - 1].repeat(3 - len);
+}
