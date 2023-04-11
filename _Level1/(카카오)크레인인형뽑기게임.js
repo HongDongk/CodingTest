9c73832a3e1f7a33977a23e85a6e434ff4057c48
Dongkeun <hongdongk@naver.com>
2023-04-11 15:03:55 +0900

    Create (카카오)크레인인형뽑기게임.js

---
+++ _Level1/(카카오)크레인인형뽑기게임.js
@@ -0,0 +1,22 @@
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+
+  for (let i of moves) {
+    let now = i - 1;
+    for (let j of board) {
+      if (j[now] !== 0) {
+        if (stack[stack.length - 1] === j[now]) {
+          stack.pop();
+          answer += 2;
+        } else {
+          stack.push(j[now]);
+        }
+        j[now] = 0;
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
