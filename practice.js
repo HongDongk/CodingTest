a9f11000ebc65fc460cfc4469bd69044883dda24
Dongkeun <hongdongk@naver.com>
2023-04-13 14:46:11 +0900

    햄버거만들기

--- practice.js
+++ practice.js
@@ -1,5 +1,4 @@
-function solution(babbling) {
-  return babbling.replace(/(\.)\1+/g, ".");
-}
+const months = ["Jan", "March", "April", "June"];
 
-console.log(solution("...sf...s"));
+months.splice(-1); // 뒤에서 2번째인덱스 값부터 삭제
+console.log(months);
