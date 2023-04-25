63c35121f63ebf541df2625b023f1ee78d0c3aff
Dongkeun <hongdongk@naver.com>
2023-04-25 17:06:33 +0900

    Update 게임맵최단거리.js

--- 완전탐색/DFS&BFS/게임맵최단거리.js
+++ 완전탐색/DFS&BFS/게임맵최단거리.js
@@ -1,38 +1,32 @@
 // 최단거리 - BFS(큐)
 
 function solution(maps) {
-    let answer = 1;
-    let visited = Array(maps.length).fill().map(()=> Array(maps[0].length).fill(1));
-    let que = [[0,0]];
-    const maxx = maps.length;
-    const maxy = maps[0].length;
-    
-    const dx = [-1,1,0,0];
-    const dy = [0,0,-1,1];
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  const queue = [[0, 0, 1]];
 
-    while(que.length > 0){
-        let size = que.length;
-    
-        for(let i = 0; i < size; i++) {
-            let [x,y] = que.shift();
-            
-            for(let j=0; j<4; j++){
-                let mx = x + dx[j];
-                let my = y + dy[j];
-                
-                if (mx>=0 && my>=0 && mx<maxx && my<maxy && maps[mx][my] === 1 && visited[mx][my] === 1){
-                    if(mx === maxx - 1 && my === maxy - 1){
-                        return answer + 1;
-                    }
-                    else{
-                        que.push([mx,my]);
-                        visited[mx][my] = 0;
-                    }
-                }
-            }
-        }
-        answer++;
+  while (queue.length > 0) {
+    const cur = queue.shift();
+    if (cur[0] === maps.length - 1 && cur[1] === maps[0].length - 1) {
+      return cur[2];
     }
-    
-    return -1;
+
+    for (let i = 0; i < 4; i++) {
+      const nx = cur[0] + dx[i];
+      const ny = cur[1] + dy[i];
+
+      if (
+        nx >= 0 &&
+        ny >= 0 &&
+        nx < maps.length &&
+        ny < maps[0].length &&
+        maps[nx][ny] === 1
+      ) {
+        maps[nx][ny] = 0;
+        queue.push([nx, ny, cur[2] + 1]);
+      }
+    }
+  }
+
+  return -1;
 }
