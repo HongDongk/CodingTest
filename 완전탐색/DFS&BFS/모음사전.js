a35969b244afc4efa4df11f09e75fb9128130bca
Dongkeun <hongdongk@naver.com>
2023-04-26 13:46:20 +0900

    모음사전

---
+++ 완전탐색/DFS&BFS/모음사전.js
@@ -0,0 +1,19 @@
+// 재귀함수!!
+
+function solution(word) {
+  let arr = ["A", "E", "I", "O", "U"];
+  let idx = 0;
+  let answer = {};
+
+  function dfs(word, count) {
+    if (count > 5) return;
+    answer[word] = idx++;
+    for (let i of arr) {
+      dfs(word + i, count + 1);
+    }
+  }
+
+  dfs("", 0);
+
+  return answer[word];
+}
