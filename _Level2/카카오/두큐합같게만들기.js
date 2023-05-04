692a6c1d0d07022109923dad49a6d7abd21dd12f
Dongkeun <hongdongk@naver.com>
2023-05-04 15:10:35 +0900

    두큐합같게만들기

---
+++ _Level2/카카오/두큐합같게만들기.js
@@ -0,0 +1,19 @@
+// 투 포인터
+
+function solution(queue1, queue2) {
+  let sum1 = queue1.reduce((a, b) => a + b, 0);
+  let sum2 = queue2.reduce((a, b) => a + b, 0);
+  const half = (sum1 + sum2) / 2;
+  const q = [...queue1, ...queue2];
+  let q1Pointer = 0;
+  let q2Pointer = queue1.length;
+
+  for (let cnt = 0; cnt < queue1.length * 3; cnt++) {
+    if (sum1 === half) {
+      return cnt;
+    }
+    sum1 = sum1 >= half ? sum1 - q[q1Pointer++] : sum1 + q[q2Pointer++];
+  }
+
+  return -1;
+}
--- practice.js
+++ practice.js
@@ -1,5 +1,18 @@
-let arrA = [1, 23, 4, 5, 23];
+const arr = [1, 2, 3, 2, 8];
+let answer = 0;
+let start = 0;
+let end = 0;
+let result = 0;
 
-let k = 3;
+while (arr[start] !== undefined) {
+  while (result < 5 && end < arr.length) {
+    result += arr[end++];
+  }
+  if (result === 5) {
+    answer++;
+  }
+  result -= arr[start++];
+}
 
-console.log(arrA.slice(0, k).join(""));
+console.log(answer);
+// 결과 3
