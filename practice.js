5074ef08a59f21946c3ac5738666793dfd37d56b
Dongkeun <hongdongk@naver.com>
2023-05-05 22:03:38 +0900

    괄호변환

--- practice.js
+++ practice.js
@@ -1,18 +1,4 @@
-const arr = [1, 2, 3, 2, 8];
-let answer = 0;
-let start = 0;
-let end = 0;
-let result = 0;
-
-while (arr[start] !== undefined) {
-  while (result < 5 && end < arr.length) {
-    result += arr[end++];
-  }
-  if (result === 5) {
-    answer++;
-  }
-  result -= arr[start++];
-}
-
-console.log(answer);
+let str = "safs";
+str += "강아지";
+console.log(str);
 // 결과 3
