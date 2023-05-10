9129815e1be9b92b83da3fd2005587f0d04e0d50
Dongkeun <hongdongk@naver.com>
2023-05-10 15:07:10 +0900

    정규식정리

--- 그리디/체육복.js
+++ 그리디/체육복.js
@@ -1,32 +1,22 @@
-// 객체를 이용해서 풀기!!
-
 function solution(n, lost, reserve) {
-    let student = {};
-    let answer = 0;
-    
-    for(let i =1; i<=n; i++){
-        student[i] = 1;
+  let arr = new Array(n).fill(1);
+
+  for (let i of lost) {
+    arr[i - 1]--;
+  }
+  for (let i of reserve) {
+    arr[i - 1]++;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] >= 2 && arr[i] === 0) {
+      arr[i - 1] -= 1;
+      arr[i] += 1;
+    } else if (arr[i - 1] === 0 && arr[i] >= 2) {
+      arr[i] -= 1;
+      arr[i - 1] += 1;
     }
-    
-    lost.forEach((a) => student[a] -= 1);
-    reserve.forEach((a) => student[a] += 1);
-    
-    for(let i=1; i<=n; i++){
-        if(student[i] === 2 && student[i-1] === 0){
-            student[i-1]+=1;
-            student[i]-=1;
-        }
-        else if(student[i] === 2 && student[i+1] === 0){
-            student[i+1]+=1;
-            student[i]-=1;
-        }
-    }
-    
-    for(let i in student){
-        if (student[i] >= 1){
-            answer++;
-        }
-    }
-    
-    return answer;
+  }
+
+  return arr.filter((a) => a >= 1).length;
 }
