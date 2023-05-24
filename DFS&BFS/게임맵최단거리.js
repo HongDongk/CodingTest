70a533fb098c425d211da6f936e2b66ceae2d43b
Dongkeun <hongdongk@naver.com>
2023-05-24 14:40:24 +0900

    미로탈출

--- DFS&BFS/게임맵최단거리.js
+++ DFS&BFS/게임맵최단거리.js
@@ -1,31 +1,36 @@
 // 최단거리 - BFS(큐)
 
 function solution(maps) {
+  let answer = 1;
+  let queue = [[0, 0]];
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
-  const queue = [[0, 0, 1]];
 
   while (queue.length > 0) {
-    const cur = queue.shift();
-    if (cur[0] === maps.length - 1 && cur[1] === maps[0].length - 1) {
-      return cur[2];
-    }
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let [x, y] = queue.shift();
 
-    for (let i = 0; i < 4; i++) {
-      const nx = cur[0] + dx[i];
-      const ny = cur[1] + dy[i];
+      for (let j = 0; j < 4; j++) {
+        let nx = x + dx[j];
+        let ny = y + dy[j];
 
-      if (
-        nx >= 0 &&
-        ny >= 0 &&
-        nx < maps.length &&
-        ny < maps[0].length &&
-        maps[nx][ny] === 1
-      ) {
-        maps[nx][ny] = 0;
-        queue.push([nx, ny, cur[2] + 1]);
+        if (
+          nx >= 0 &&
+          nx < maps.length &&
+          ny >= 0 &&
+          ny < maps[0].length &&
+          maps[nx][ny] !== 0
+        ) {
+          if (nx === maps.length - 1 && ny === maps[0].length - 1) {
+            return answer + 1;
+          }
+          queue.push([nx, ny]);
+          maps[nx][ny] = 0;
+        }
       }
     }
+    answer++;
   }
 
   return -1;
---
+++ DFS&BFS/미로탈출.js
@@ -0,0 +1,60 @@
+// 두번 BFS(큐) 사용하면 됌
+
+function bfs(start, arr, target) {
+  let answer = 0;
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  const queue = [start];
+  arr[start[0]][start[1]] = "X";
+
+  while (queue.length > 0) {
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const [x, y] = queue.shift();
+
+      for (let j = 0; j < 4; j++) {
+        let nx = x + dx[j];
+        let ny = y + dy[j];
+
+        if (
+          nx >= 0 &&
+          nx < arr.length &&
+          ny >= 0 &&
+          ny < arr[0].length &&
+          arr[nx][ny] !== "X"
+        ) {
+          if (arr[nx][ny] === target) {
+            return answer + 1;
+          }
+          queue.push([nx, ny]);
+          arr[nx][ny] = "X";
+        }
+      }
+    }
+    answer++;
+  }
+
+  return -1;
+}
+
+function solution(maps) {
+  let start = [];
+  let lever = [];
+
+  const maps1 = maps.map((a) => a.split(""));
+  const maps2 = maps.map((a) => a.split(""));
+
+  for (let i = 0; i < maps.length; i++) {
+    for (let j = 0; j < maps[i].length; j++) {
+      if (maps[i][j] === "S") start = [i, j];
+      if (maps[i][j] === "L") lever = [i, j];
+    }
+  }
+
+  const a = bfs(start, maps1, "L"); // 시작점에서 끝점
+  const b = bfs(lever, maps2, "E"); // 레버에서 끝점
+
+  if (a === -1 || b === -1) return -1; // 둘중에 하나라도 못가면 -1
+
+  return a + b;
+}
