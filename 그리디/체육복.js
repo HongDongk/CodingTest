2b2006b1362009ec8aa7ae7061ea9d009ec20730
Dongkeun <hongdongk@naver.com>
2023-06-12 15:54:24 +0900

    Update 체육복.js

--- 그리디/체육복.js
+++ 그리디/체육복.js
@@ -1,20 +1,17 @@
 function solution(n, lost, reserve) {
   let arr = new Array(n).fill(1);
 
-  for (let i of lost) {
-    arr[i - 1]--;
-  }
-  for (let i of reserve) {
-    arr[i - 1]++;
-  }
+  lost.map((a) => arr[a - 1]--);
+  reserve.map((a) => arr[a - 1]++);
 
+  // 여기부분 조심
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] >= 2 && arr[i] === 0) {
-      arr[i - 1] -= 1;
-      arr[i] += 1;
+      arr[i - 1]--;
+      arr[i]++;
     } else if (arr[i - 1] === 0 && arr[i] >= 2) {
-      arr[i] -= 1;
-      arr[i - 1] += 1;
+      arr[i]--;
+      arr[i - 1]++;
     }
   }
 
