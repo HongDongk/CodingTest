5f520b1a0059f7025ff01cb5e2312170ea170e03
Dongkeun <hongdongk@naver.com>
2023-06-12 15:18:26 +0900

    Update 구명보트.js

--- 그리디/구명보트.js
+++ 그리디/구명보트.js
@@ -1,20 +1,22 @@
+// 합쳐서 조건보다 작은 값을 찾을 때는 배열의 인덱스를 활용해본다!
+
 function solution(people, limit) {
-    let answer = 0;
-    people.sort((a,b) => b-a);
-    let a = 0;
-    let b = people.length-1;
-    
-    while(a<b){
-        if(people[a] + people[b]>limit){
-            a++;        
-        } else{
-            a++;
-            b--;
-        }
-        answer++;
+  let answer = 0;
+  people.sort((a, b) => b - a);
+  let a = 0;
+  let b = people.length - 1;
+
+  while (a < b) {
+    if (people[a] + people[b] > limit) {
+      a++;
+    } else {
+      a++;
+      b--;
     }
-    
-    if(a===b) answer++;
-    
-    return answer;
+    answer++;
+  }
+
+  if (a === b) answer++; // 한명 남았을 때 1 추가
+
+  return answer;
 }
