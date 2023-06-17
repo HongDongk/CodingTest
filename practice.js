c069c09497076339ef17ba00fc3471a64ca79fa6
Dongkeun <hongdongk@naver.com>
2023-06-17 21:38:02 +0900

    압축다시풀기

--- practice.js
+++ practice.js
@@ -1,28 +1,3 @@
-const x = 100;
+const a = "abcdef";
 
-function outer() {
-  let x = 0;
-
-  function xplus() {
-    x = x + 1;
-  }
-
-  function printx() {
-    console.log(x);
-  }
-
-  return xplus;
-}
-
-const xclosure = outer();
-
-xclosure.printx(); // 0
-xclosure.xplus();
-xclosure.printx(); // 1
-xclosure.xplus();
-xclosure.printx(); // 2
-// outer().printx();
-// outer().xplus();
-// outer().printx();
-// outer().xplus();
-// outer().printx();
+console.log(a.slice(0, -1));
---
+++ 구현/카카오/[3차]압축.js
@@ -0,0 +1,48 @@
+// 문자열 구현
+
+function solution(msg) {
+  let words = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  let temp = "";
+  let answer = [];
+
+  for (let i of msg) {
+    temp += i;
+    if (!words.includes(temp)) {
+      answer.push(words.indexOf(temp.slice(0, -1)) + 1);
+      words.push(temp);
+      temp = i;
+    }
+  }
+
+  if (temp) answer.push(words.indexOf(temp) + 1);
+
+  return answer;
+}
