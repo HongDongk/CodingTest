70fdc3bd54e04102919726c99acc04cb903de827
Dongkeun <hongdongk@naver.com>
2023-07-10 22:41:19 +0900

    다트게임다시풀기 (해결)

--- practice.js
+++ practice.js
@@ -1,5 +1,3 @@
-const string = "정규식123abc";
-
-console.log(string.replace(/[123]/g, "")); // a,b,c 제거 => '정규식123_'
-console.log(string.replace(/[^123]/g, "")); // 1,2,3만 남기고 제거 => '123'
-console.log(string.replace(/정규/g, "김"));
+const attached = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const attached2 = [1, 2, 3, 4, 5];
+console.log([...attached, ...attached2]);
