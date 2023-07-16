7adc7532ca13245e044a0b9a87e9fcdb4a9b6ddc
Dongkeun <hongdongk@naver.com>
2023-07-16 17:59:08 +0900

    네트워크다시풀기(실패)

--- DFS&BFS/네트워크.js
+++ DFS&BFS/네트워크.js
@@ -1,24 +1,24 @@
 // 네트워크 - DFS(재귀함수)
 
 function solution(n, computers) {
-    let visited = new Array(n).fill(false);
-    let answer = 0;
-    
-    function dfs(i) {
-        visited[i] = true;
-        for (let j=0; j<computers[i].length; j++){
-            if(visited[j] === false && computers[i][j] ===1 ){
-                dfs(j);
-            }
-        }
+  let visited = new Array(n).fill(false);
+  let answer = 0;
+
+  function dfs(i) {
+    visited[i] = true;
+    for (let j = 0; j < computers[i].length; j++) {
+      if (visited[j] === false && computers[i][j] === 1) {
+        dfs(j);
+      }
     }
-    
-    for (let i in computers){
-        if(visited[i] === false){
-            dfs(i);
-            answer+=1;
-        }
+  }
+
+  for (let i = 0; i < computers.length; i++) {
+    if (visited[i] === false) {
+      dfs(i);
+      answer += 1;
     }
-    
-    return answer;
+  }
+
+  return answer;
 }
