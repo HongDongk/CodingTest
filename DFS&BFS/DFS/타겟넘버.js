ff5c4c5c6d1ba822f3390d09f5ebcbadf325f3ac
Dongkeun <hongdongk@naver.com>
2023-08-02 22:54:19 +0900

    타겟넘버다시풀기

--- DFS&BFS/DFS/타겟넘버.js
+++ DFS&BFS/DFS/타겟넘버.js
@@ -1,20 +1,20 @@
 // 모든조합 - DFS(재귀함수)
 
 function solution(numbers, target) {
-    let answer = 0;
-    
-    function dfs(count, value){
-        if(count<numbers.length){
-            dfs(count+1, value+numbers[count]);
-            dfs(count+1, value-numbers[count]);
-        } else{
-            if(value === target){
-                answer+=1;
-            }
-        } 
+  let answer = 0;
+
+  function dfs(count, value) {
+    if (count < numbers.length) {
+      dfs(count + 1, value + numbers[count]);
+      dfs(count + 1, value - numbers[count]);
+    } else {
+      if (value === target) {
+        answer++;
+      }
     }
-    
-    dfs(0, 0);
-    
-    return answer;
+  }
+
+  dfs(0, 0);
+
+  return answer;
 }
