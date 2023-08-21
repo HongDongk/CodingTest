9f1286a1ded985236b5f746c13ed81025ecfc733
Dongkeun <hongdongk@naver.com>
2023-08-21 22:27:48 +0900

    Update 2 x n타일링.js

--- DP/2 x n타일링.js
+++ DP/2 x n타일링.js
@@ -1,11 +1,12 @@
-// 피보나치수열을 DP로 구현
+// 타일을 채워나가는 규칙을 살펴보면 피보나치수열과 같다.
+// 피보나치수열을 dp로 구현
 
 function solution(n) {
-  let answer = 0;
-  let dp = Array(n).fill(0);
+  let dp = new Array(n).fill(0);
   dp[0] = 1;
   dp[1] = 2;
-  for (var i = 2; i < n; i++) {
+
+  for (let i = 2; i < n; i++) {
     dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
   }
 
