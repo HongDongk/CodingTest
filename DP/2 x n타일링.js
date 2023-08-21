b9862d750edcff483706deca09b9886efee5a700
Dongkeun <hongdongk@naver.com>
2023-08-21 22:32:15 +0900

    Update 2 x n타일링.js

--- DP/2 x n타일링.js
+++ DP/2 x n타일링.js
@@ -2,9 +2,7 @@
 // 피보나치수열을 dp로 구현
 
 function solution(n) {
-  let dp = new Array(n).fill(0);
-  dp[0] = 1;
-  dp[1] = 2;
+  let dp = [1, 2];
 
   for (let i = 2; i < n; i++) {
     dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
