716c3966288a2d1568676891ef783a1e76151d62
Dongkeun <hongdongk@naver.com>
2023-08-28 22:59:57 +0900

    Update 마법의엘리베이터.js

--- DFS&BFS/DFS/마법의엘리베이터.js
+++ DFS&BFS/DFS/마법의엘리베이터.js
@@ -3,14 +3,14 @@
 function solution(storey) {
   let answer = 1000000;
 
-  function dfs(num, counter) {
-    if (counter >= answer) return; // 최솟값보다 큰 것 리턴
-    if (num === 0) {
-      answer = counter;
+  function dfs(current, count) {
+    if (count >= answer) return;
+    if (current === 0) {
+      answer = count;
     } else {
-      let res = num % 10;
-      dfs(Math.floor(num / 10), counter + res); // 층을 내려갔을 경우
-      dfs(Math.floor(num / 10) + 1, counter + 10 - res); // 층을 올라갔을 경우
+      let res = current % 10;
+      dfs(Math.floor(current / 10), count + res);
+      dfs(Math.floor(current / 10) + 1, count + 10 - res);
     }
   }
 
