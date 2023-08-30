9e19dfc9bb0a63362abba84110b17585d1a8e0fc
Dongkeun <hongdongk@naver.com>
2023-08-30 23:03:31 +0900

    Update 모음사전.js

--- DFS&BFS/DFS/모음사전.js
+++ DFS&BFS/DFS/모음사전.js
@@ -3,19 +3,19 @@
 // answer = {"":0,"A":1,"AA":2,"AAA":3,"AAAA":4,"AAAAA":5,"AAAAE":6,"AAAAI":7,"AAAAO":8,"AAAAU":9,"AAAE":10,"AAAEA":11,"AAAEE":12,"AAAEI":13}
 
 function solution(word) {
-  let arr = ["A", "E", "I", "O", "U"];
   let idx = 0;
   let answer = {};
+  const alphabet = ["A", "E", "I", "O", "U"];
 
-  function dfs(word, count) {
+  function dfs(count, word) {
     if (count > 5) return;
     answer[word] = idx++;
-    for (let i of arr) {
-      dfs(word + i, count + 1);
+    for (let i of alphabet) {
+      dfs(count + 1, word + i);
     }
   }
 
-  dfs("", 0);
+  dfs(0, "");
 
   return answer[word];
 }
