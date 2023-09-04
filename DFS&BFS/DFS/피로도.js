88232aa9ff2868bd2df77c88a59547a9c8c2101f
Dongkeun <hongdongk@naver.com>
2023-09-04 23:10:28 +0900

    Update 피로도.js

--- DFS&BFS/DFS/피로도.js
+++ DFS&BFS/DFS/피로도.js
@@ -1,16 +1,17 @@
 // 모든조합 - DFS(재귀함수)
+// 값을 배열에 다 넣어서 Math.max하면 시간초과남
 
 function solution(k, dungeons) {
   let answer = 0;
   let visited = new Array(dungeons.length).fill(false); // 방문했는지 안했는지 확인필요!
 
-  function dfs(power, cnt) {
-    answer = Math.max(answer, cnt);
+  function dfs(k, n) {
+    answer = Math.max(answer, n);
 
     for (let i = 0; i < dungeons.length; i++) {
-      if (dungeons[i][0] <= power && !visited[i]) {
+      if (dungeons[i][0] <= k && !visited[i]) {
         visited[i] = true;
-        dfs(power - dungeons[i][1], cnt + 1);
+        dfs(k - dungeons[i][1], n + 1);
         visited[i] = false;
       }
     }
