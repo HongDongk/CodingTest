a67c2742e07b9bafae7ca52365fe1c80718bf801
Dongkeun <hongdongk@naver.com>
2024-03-20 21:20:23 +0900

    Update No.2 가장큰정사각형찾기.js

--- DP/No.2 가장큰정사각형찾기.js
+++ DP/No.2 가장큰정사각형찾기.js
@@ -2,7 +2,7 @@
 
 function solution(board) {
   let answer = 0;
-  if (row <= 1 || column <= 1) return 1;
+  if (board.length <= 1 || board[0].length <= 1) return 1;
 
   for (let i = 1; i < board.length; i++) {
     for (let j = 1; j < board[0].length; j++) {
