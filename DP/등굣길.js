92fa82640fadf8fc153d94b10925081213cad186
Dongkeun <hongdongk@naver.com>
2024-03-22 15:26:20 +0900

    등굣길다시풀기

--- DP/등굣길.js
+++ DP/등굣길.js
@@ -1,19 +1,30 @@
 // 점화식: M[n][m] = M[n-1][m] + M[n][m-1]
 
 function solution(m, n, puddles) {
-    let dp = new Array(n).fill().map((a) => new Array(m).fill(0));
-    dp[0][0] = 1;
+  let dp = new Array(n).fill().map((a) => new Array(m).fill(0));
+  dp[0][0] = 1;
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (i === 0 && j === 0) continue; // 첫번째 위치 제외
-            if (puddles.some(([x, y]) => x === j + 1 && y === i + 1)) {
-                dp[i][j] = 0; // 웅덩이일때 0으로 설정
-            } else {
-                dp[i][j] = ((i > 0 ? dp[i - 1][j] : 0) + (j > 0 ? dp[i][j - 1] : 0)) % 1000000007;
-            }
-        }
+  for (let [x, y] of puddles) {
+    dp[y - 1][x - 1] = -1;
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (i === 0 && j === 0) continue;
+      if (dp[i][j] === -1) continue;
+
+      let top = 0;
+      let left = 0;
+      if (i - 1 >= 0 && dp[i - 1][j] !== -1) {
+        top = dp[i - 1][j] % 1000000007;
+      }
+      if (j - 1 >= 0 && dp[i][j - 1] !== -1) {
+        left = dp[i][j - 1] % 1000000007;
+      }
+
+      dp[i][j] = top + left;
     }
+  }
 
-    return dp[n - 1][m - 1];
+  return dp[n - 1][m - 1] % 1000000007;
 }
