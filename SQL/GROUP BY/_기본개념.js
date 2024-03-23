d6272144ca2f721482927b2990da2e840efc3b4d
Dongkeun <hongdongk@naver.com>
2024-03-23 13:47:14 +0900

    group by 추가

---
+++ SQL/GROUP BY/_기본개념.js
@@ -0,0 +1,9 @@
+// 1. SELECT a, COUNT(*) FROM A GROUP BY a : A테이블을 a컬럼 기준으로 나눳을때 각각갯수
+
+// 2. GROUP BY a HAVING 문제조건 : a컬럼으로 나눈 Group에서 문제조건에 맞는 데이터만 선별
+
+// 3.
+
+// 4.
+
+// 5. WHERE (a LIKE '%제시문자%') : 제시문자가 포함된 a컬럼 선별
