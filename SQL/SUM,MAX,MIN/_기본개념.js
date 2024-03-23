8e4a658022150c9f7b6d3f6b11f48d0701ea84eb
Dongkeun <hongdongk@naver.com>
2024-03-23 10:29:44 +0900

    sum,max,min 문제추가

---
+++ SQL/SUM,MAX,MIN/_기본개념.js
@@ -0,0 +1,9 @@
+// 1. SELECT MAX(a컬럼) : a컬럼 최댓값
+
+// 2. SELECT MIN(a컬럼) : a컬럼 최솟값
+
+// 3. SELECT SUM(a컬럼) : a컬럼 총합
+
+// 4. SELECT COUNT(*) FROM a : a테이블 총 컬럼갯수
+
+// 5. SELECT DISTINCT a컬럼 : a컬럼에서 중복된 것 제거한 결과
