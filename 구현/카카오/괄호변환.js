52a0713d26b0f2befd492c4a1ca7757d6c69f51d
Dongkeun <hongdongk@naver.com>
2024-03-28 15:24:28 +0900

    괄호변환다시풀기

--- 구현/카카오/괄호변환.js
+++ 구현/카카오/괄호변환.js
@@ -1,15 +1,11 @@
-// 순서대로 구현하기
-
 function solution(p) {
-  if (p.length === 0) return ""; // 1
-  let u = "";
-  let v = "";
+  if (p.length === 0) return ''; // 1
+  let u = '';
+  let v = '';
   let count = 0;
 
   for (let i = 0; i < p.length; i++) {
-    // 2
-    p[i] === "(" ? count++ : count--;
-
+    p[i] === '(' ? count++ : count--;
     if (count === 0) {
       u = p.slice(0, i + 1);
       v = p.slice(i + 1);
@@ -18,12 +14,11 @@ function solution(p) {
   }
 
   for (let i = 0; i < u.length; i++) {
-    u[i] === "(" ? count++ : count--;
-
+    u[i] === '(' ? count++ : count--;
     if (count < 0) {
       let str = `(${solution(v)})`; // 4-1 ~ 4-3
       for (let j = 1; j < u.length - 1; j++) {
-        u[j] === "(" ? (str += ")") : (str += "("); // 4-4
+        u[j] === '(' ? (str += ')') : (str += '('); // 4-4
       }
       return str; // 4-5
     }
