55f0afa63c04a0232748014e9d0f87a6fbfd0871
Dongkeun <hongdongk@naver.com>
2024-05-27 22:54:18 +0900

    Update 게임맵최단거리.js

--- DFS&BFS/BFS/게임맵최단거리.js
+++ DFS&BFS/BFS/게임맵최단거리.js
@@ -1,20 +1,24 @@
+// dx는 y좌표, dy는 x좌표의 이동
+
 function solution(maps) {
   let answer = 1;
   let queue = [[0, 0]];
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
+  maps[0][0] = 0;
+
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
 
   while (queue.length) {
     let len = queue.length;
-    for (let i = 0; i < len; i++) {
-      let [x, y] = queue.shift();
-      for (let j = 0; j < 4; j++) {
-        let nx = x + dx[j];
-        let ny = y + dy[j];
-        if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && maps[nx][ny] !== 0) {
-          if (nx === maps.length - 1 && ny === maps[0].length - 1) return answer + 1;
-          queue.push([nx, ny]);
+    for (let w = 0; w < len; w++) {
+      let [cx, cy] = queue.shift();
+      if (cx === maps.length - 1 && cy === maps[0].length - 1) return answer;
+      for (let i = 0; i < 4; i++) {
+        let nx = cx + dx[i];
+        let ny = cy + dy[i];
+        if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && maps[nx][ny] === 1) {
           maps[nx][ny] = 0;
+          queue.push([nx, ny]);
         }
       }
     }
