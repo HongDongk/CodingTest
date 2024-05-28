a71775d4440fce726772d4edf9cfad4122cf62f3
Dongkeun <hongdongk@naver.com>
2024-05-28 22:42:51 +0900

    Update 무인도여행.js

--- DFS&BFS/DFS/무인도여행.js
+++ DFS&BFS/DFS/무인도여행.js
@@ -1,19 +1,17 @@
 function solution(maps) {
-  let temp = 0;
+  maps = maps.map((a) => a.split(''));
   let answer = [];
-  maps = maps.map((n) => n.split(''));
+  let sum = 0;
   let dx = [-1, 0, 1, 0];
   let dy = [0, 1, 0, -1];
 
   function DFS(x, y) {
-    if (maps[x][y] !== 'X') {
-      temp += Number(maps[x][y]);
-      maps[x][y] = 'X';
-    }
+    maps[x][y] = 'X';
     for (let i = 0; i < 4; i++) {
       let nx = x + dx[i];
       let ny = y + dy[i];
       if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && maps[nx][ny] !== 'X') {
+        sum += Number(maps[nx][ny]);
         DFS(nx, ny);
       }
     }
@@ -22,13 +20,13 @@ function solution(maps) {
   for (let i = 0; i < maps.length; i++) {
     for (let j = 0; j < maps[0].length; j++) {
       if (maps[i][j] !== 'X') {
+        sum += Number(maps[i][j]);
         DFS(i, j);
-        answer.push(temp);
-        temp = 0;
+        answer.push(sum);
+        sum = 0;
       }
     }
   }
 
-  DFS(0, 0);
   return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
