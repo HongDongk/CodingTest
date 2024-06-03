f6c1024b0c6875a5b18989d3020539c4718a1f21
Dongkeun <hongdongk@naver.com>
2024-06-03 22:54:25 +0900

    불량사용자

--- DFS&BFS/DFS/카카오_불량사용자.js
+++ DFS&BFS/DFS/카카오_불량사용자.js
@@ -1,7 +1,6 @@
 function solution(user_id, banned_id) {
   let answer = [];
   let check = new Array(user_id.length).fill(false);
-  let temp = [];
 
   function analyze(a, b) {
     if (a.length !== b.length) return false;
@@ -14,10 +13,9 @@ function solution(user_id, banned_id) {
   function DFS(L, temp) {
     if (L === banned_id.length) {
       answer.push(temp.slice());
-      temp = [];
     } else {
       for (let i = 0; i < user_id.length; i++) {
-        if (!check[i] && analyze(banned_id[L], user_id[i])) {
+        if (analyze(banned_id[L], user_id[i]) && !check[i]) {
           check[i] = true;
           temp.push(user_id[i]);
           DFS(L + 1, temp);
@@ -28,11 +26,11 @@ function solution(user_id, banned_id) {
     }
   }
 
-  DFS(0, temp);
+  DFS(0, []);
 
   answer = answer.map((a) => a.sort().join(''));
 
-  const result = new Set(answer);
+  let result = new Set(answer);
 
   return result.size;
 }
