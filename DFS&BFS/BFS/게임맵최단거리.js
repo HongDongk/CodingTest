b82e9194e0b6d542379d2df150d33de041efc5d0
Dongkeun <hongdongk@naver.com>
2024-06-03 21:30:20 +0900

    게임맵최단거리

--- DFS&BFS/BFS/게임맵최단거리.js
+++ DFS&BFS/BFS/게임맵최단거리.js
@@ -12,11 +12,11 @@ function solution(maps) {
     let len = queue.length;
     for (let w = 0; w < len; w++) {
       let [cx, cy] = queue.shift();
-      if (cx === maps.length - 1 && cy === maps[0].length - 1) return answer;
       for (let i = 0; i < 4; i++) {
         let nx = cx + dx[i];
         let ny = cy + dy[i];
         if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && maps[nx][ny] === 1) {
+          if (nx === maps.length - 1 && ny === maps[0].length - 1) return answer + 1;
           maps[nx][ny] = 0;
           queue.push([nx, ny]);
         }
---
+++ DFS&BFS/DFS/(해결)여행경로.js
@@ -0,0 +1,27 @@
+function solution(tickets) {
+  let answer = [];
+  let temp = ['ICN'];
+  tickets = tickets.sort();
+  let check = new Array(tickets.length).fill(false);
+
+  function DFS(start, L) {
+    if (L === tickets.length) {
+      return answer.push(temp.slice()); // 얕은 복사주의!
+    } else {
+      for (let i = 0; i < tickets.length; i++) {
+        let [s, e] = tickets[i];
+        if (s === start && !check[i]) {
+          temp.push(e);
+          check[i] = true;
+          DFS(e, L + 1);
+          temp.pop(e);
+          check[i] = false;
+        }
+      }
+    }
+  }
+
+  DFS('ICN', 0);
+
+  return answer.sort()[0];
+}
