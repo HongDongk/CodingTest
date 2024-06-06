0b87fcc75e3a4807d9655c442560f0358c562f92
Dongkeun <hongdongk@naver.com>
2024-06-06 22:52:52 +0900

    Update 카카오_이모티콘할인행사.js

--- DFS&BFS/DFS/카카오_이모티콘할인행사.js
+++ DFS&BFS/DFS/카카오_이모티콘할인행사.js
@@ -1,53 +1,47 @@
-// 시간 오래걸림 + 문제 이해하는데 오래걸림
+// 3중 for문을 사용해도 풀림!
 
 function solution(users, emoticons) {
-  let answer = [0, 0];
   let discount = [10, 20, 30, 40];
-  let temp = new Array(emoticons.length).fill(false);
-  let permutation = [];
+  let temp = [];
+  let result = [];
+  let answer = [0, 0];
 
-  // 할인율에 대한 중복 순열구하기
   function DFS(L) {
-    if (L === emoticons.length) {
-      permutation.push(temp.slice());
-    } else {
-      for (let i = 0; i < 4; i++) {
-        temp[L] = discount[i];
+    if (L === emoticons.length) result.push(temp.slice());
+    else {
+      for (let i of discount) {
+        temp.push(i);
         DFS(L + 1);
+        temp.pop();
       }
     }
   }
 
   DFS(0);
 
-  for (let arr of permutation) {
+  for (let arr of result) {
     let emprice = 0;
     let emplus = 0;
     for (let user of users) {
       let temp = 0;
-      let temp2 = 0;
       let [minper, maxprice] = user;
 
       for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= minper) {
-          temp += ((100 - arr[i]) / 100) * emoticons[i];
-        }
+        if (arr[i] >= minper) temp += ((100 - arr[i]) / 100) * emoticons[i];
       }
 
       if (temp >= maxprice) {
-        temp2 = 1;
-        temp = 0;
+        emplus += 1;
+      } else {
+        emprice += temp;
       }
-
-      emprice += temp;
-      emplus += temp2;
     }
+
     if (answer[0] < emplus) {
       answer = [emplus, emprice];
-    } else if (answer[0] === emplus) {
-      if (answer[1] < emprice) {
-        answer = [emplus, emprice];
-      }
+    }
+    if (answer[0] === emplus) {
+      answer = [emplus, Math.max(answer[1], emprice)];
     }
   }
 
