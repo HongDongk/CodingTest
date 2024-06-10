7f361a1dc72ecb8a01b08c98640ceb08538068c0
Dongkeun <hongdongk@naver.com>
2024-06-10 21:58:08 +0900

    Update 2 x n타일링.js

--- DP/2 x n타일링.js
+++ DP/2 x n타일링.js
@@ -1,9 +1,11 @@
 function solution(n) {
-  let dp = [1, 2];
+  let dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
 
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i <= n; i++) {
     dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
   }
 
-  return dp[n - 1];
+  return dp[n];
 }
