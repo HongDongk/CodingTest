b9782c8c7a4548464b4497aada7c7231ce1d3ea0
Dongkeun <hongdongk@naver.com>
2024-06-13 23:07:01 +0900

    Create 정수 삼각형.js

---
+++ DP/정수 삼각형.js
@@ -0,0 +1,11 @@
+// 밑에서부터 올라간다!
+
+function solution(triangle) {
+  for (let i = triangle.length - 2; i >= 0; i--) {
+    for (let j = 0; j < triangle[i].length; j++) {
+      triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    }
+  }
+
+  return triangle[0][0];
+}
