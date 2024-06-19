8732d143707d494c7ddf0616a3aab649fa1f7639
Dongkeun <hongdongk@naver.com>
2024-06-19 22:07:29 +0900

    Update 가장큰정사각형찾기.js

--- DP/가장큰정사각형찾기.js
+++ DP/가장큰정사각형찾기.js
@@ -1,21 +1,17 @@
-// (왼쪽, 왼쪽위대각선, 위쪽의 최솟값) + 1 => 해당 index에서의 최대정사각형넓이
+// 해당 index에서의 최대정사각형넓이 : (왼쪽, 왼쪽위대각선, 위쪽의 최솟값) + 1
 
 function solution(board) {
-  let answer = 0;
   if (board.length <= 1 || board[0].length <= 1) return 1;
 
+  let max = 0;
   for (let i = 1; i < board.length; i++) {
     for (let j = 1; j < board[0].length; j++) {
-      if (board[i][j] > 0) {
-        const up = board[i - 1][j];
-        const left = board[i][j - 1];
-        const cross = board[i - 1][j - 1];
-        const minNum = Math.min(up, left, cross);
-        board[i][j] = minNum + 1;
-        answer = Math.max(answer, board[i][j]);
+      if (board[i][j] === 1) {
+        board[i][j] = Math.min(board[i - 1][j], board[i - 1][j - 1], board[i][j - 1]) + 1;
       }
+      max = Math.max(max, board[i][j]);
     }
   }
 
-  return answer * answer;
+  return max * max;
 }
