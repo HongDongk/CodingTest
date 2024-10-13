a0ddea53613f11381c80344334dcef3d8ec95476
Dongkeun <hongdongk@naver.com>
2024-10-13 23:07:24 +0900

    전력망다시풀기

--- DFS & BFS/전력망을둘로나누기.js
+++ DFS & BFS/전력망을둘로나누기.js
@@ -1,36 +1,35 @@
 // DFS
 function solution(n, wires) {
-  let answer = 100; // 최댓값
-
-  // 와이어 연결 정보를 담을 graph
+  let answer = 100;
+  let visited = new Array(n + 1).fill(false);
+  let count = 1;
   let graph = new Array(n + 1).fill(0).map((a) => new Array(n + 1).fill(false));
-
-  // 와이어 연결 정보 저장
-  for (let wire of wires) {
-    graph[wire[0]][wire[1]] = graph[wire[1]][wire[0]] = true;
+  for (let [a, b] of wires) {
+    graph[a][b] = true;
+    graph[b][a] = true;
   }
-
-  function DFS(arr, node) {
-    let res = 0;
-    for (let i = 1; i <= arr[node].length; i++) {
-      if (arr[node][i]) {
-        arr[node][i] = arr[i][node] = false; // 연결되어 있다면 방문 처리
-        res += 1 + DFS(arr, i);
+  function DFS(L) {
+    for (let i = 1; i <= n; i++) {
+      if (visited[i] === false && graph[L][i] === true) {
+        visited[L] = true;
+        count++;
+        DFS(i);
+        visited[L] = false;
       }
     }
-    console.log(res, node);
-    return res;
+    console.log('실행 끝', L, visited);
   }
 
-  // 와이어를 하나씩 끊고 끊어진 각 노드 2개에 대하여 DFS수행 후의 차이를 계산
-  for (let wire of wires) {
-    const copy = graph.map((a) => a.slice());
-    const [a, b] = wire;
-    copy[a][b] = copy[b][a] = false;
-    console.log(copy);
-    answer = Math.min(answer, Math.abs(DFS(copy, a) - DFS(copy, b)));
+  for (let [a, b] of wires) {
+    console.log([a, b]);
+    graph[a][b] = false;
+    graph[b][a] = false;
+    count = 1;
+    DFS(1);
+    graph[a][b] = true;
+    graph[b][a] = true;
+    answer = Math.min(answer, Math.abs(count - (n - count)));
   }
-
   return answer;
 }
 
