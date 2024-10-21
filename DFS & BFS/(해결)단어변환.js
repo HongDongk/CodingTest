de2378b7b8483a276909e08aff758c24c3b55f6c
Dongkeun <hongdongk@naver.com>
2024-10-21 22:42:28 +0900

    Update (해결)단어변환.js

--- DFS & BFS/(해결)단어변환.js
+++ DFS & BFS/(해결)단어변환.js
@@ -2,32 +2,29 @@
 
 function solution(begin, target, words) {
   let answer = 0;
+  let check = new Array(words.length).fill(false);
   let queue = [begin];
-  let ch = new Array(words.length).fill(false);
 
-  if (!words.includes(target)) return 0;
-
-  function checkword(a, b) {
-    let count = 0;
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) count++;
-    }
-    if (count === 1) return true;
-    return false;
-  }
-
-  while (queue.length) {
+  while (queue.length > 0) {
     let len = queue.length;
     for (let i = 0; i < len; i++) {
-      let now = queue.shift();
-      if (now === target) return answer;
+      let x = queue.shift();
+      if (x === target) return answer;
       for (let j = 0; j < words.length; j++) {
-        if (!ch[j] && checkword(now, words[j])) {
-          ch[j] = true;
-          queue.push(words[j]);
+        if (!check[j]) {
+          let count = 0;
+          for (let k = 0; k < x.length; k++) {
+            if (x[k] !== words[j][k]) count++;
+          }
+          if (count === 1) {
+            check[j] = true;
+            queue.push(words[j]);
+          }
         }
       }
     }
     answer++;
   }
+
+  return 0;
 }
