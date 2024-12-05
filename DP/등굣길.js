3a766f12a8e6da1149b2e2f377276cbe79d6673f
Dongkeun <hongdongk@naver.com>
2024-12-05 22:37:21 +0900

    dp 다시풀기

--- DP/등굣길.js
+++ DP/등굣길.js
@@ -1,28 +1,25 @@
-// 점화식: M[n][m] = M[n-1][m] + M[n][m-1]
+// 오른쪽, 아래쪽으로만 이동가능하기 때문에 dp 사용가능
 
 function solution(m, n, puddles) {
-  let dp = new Array(n).fill().map((a) => new Array(m).fill(0));
-  dp[0][0] = 1;
+  let dp = new Array(m + 1).fill(0).map((a) => new Array(n + 1).fill(0));
+  dp[1][1] = 1;
 
-  puddles.map((a) => (dp[a[0] - 1][a[1] - 1] = -1));
+  for (let puddle of puddles) {
+    dp[puddle[0]][puddle[1]] = -1;
+  }
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (i === 0 && j === 0) continue;
-      if (dp[i][j] === -1) continue;
-
-      let top = 0;
-      let left = 0;
-      if (i - 1 >= 0 && dp[i - 1][j] !== -1) {
-        top = dp[i - 1][j] % 1000000007;
-      }
-      if (j - 1 >= 0 && dp[i][j - 1] !== -1) {
-        left = dp[i][j - 1] % 1000000007;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (dp[i][j] === -1) {
+        dp[i][j] = 0;
+      } else {
+        if (i > 1) dp[i][j] += dp[i - 1][j];
+        if (j > 1) dp[i][j] += dp[i][j - 1];
       }
 
-      dp[i][j] = top + left;
+      dp[i][j] = dp[i][j] % 1000000007;
     }
   }
 
-  return dp[n - 1][m - 1] % 1000000007;
+  return dp[m][n];
 }
--- DP/정수 삼각형.js
+++ DP/정수 삼각형.js
@@ -1,11 +1,13 @@
 // 밑에서부터 올라간다!
 
 function solution(triangle) {
-  for (let i = triangle.length - 2; i >= 0; i--) {
-    for (let j = 0; j < triangle[i].length; j++) {
-      triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+  let dp = triangle.slice();
+
+  for (let i = dp.length - 2; i >= 0; i--) {
+    for (let j = 0; j < dp[i].length; j++) {
+      dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
     }
   }
 
-  return triangle[0][0];
+  return dp[0][0];
 }
