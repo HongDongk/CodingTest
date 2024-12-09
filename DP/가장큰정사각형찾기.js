b9ec12526c602f17f5c27f202dd5e154cbcd8c66
Dongkeun <hongdongk@naver.com>
2024-12-09 16:24:47 +0900

    DP 복습

--- DP/가장큰정사각형찾기.js
+++ DP/가장큰정사각형찾기.js
@@ -1,17 +1,16 @@
-// 해당 index에서의 최대정사각형넓이 : (왼쪽, 왼쪽위대각선, 위쪽의 최솟값) + 1
-
 function solution(board) {
+  let answer = 0;
+
   if (board.length <= 1 || board[0].length <= 1) return 1;
 
-  let max = 0;
   for (let i = 1; i < board.length; i++) {
     for (let j = 1; j < board[0].length; j++) {
-      if (board[i][j] === 1) {
-        board[i][j] = Math.min(board[i - 1][j], board[i - 1][j - 1], board[i][j - 1]) + 1;
+      if (board[i][j] > 0) {
+        board[i][j] = Math.min(board[i - 1][j], board[i][j - 1], board[i - 1][j - 1]) + 1;
+        answer = Math.max(answer, board[i][j]);
       }
-      max = Math.max(max, board[i][j]);
     }
   }
 
-  return max * max;
+  return answer ** 2;
 }
