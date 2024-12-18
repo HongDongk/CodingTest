b8f6491e6aa39a11024f9593d27aa98e3fc014af
Dongkeun <hongdongk@naver.com>
2024-12-18 22:51:03 +0900

    N-Queen 다시풀기

--- 백트래킹/N-Queen.js
+++ 백트래킹/N-Queen.js
@@ -2,25 +2,29 @@ function solution(n) {
   let answer = 0;
   let queens = [];
 
-  function possible(x, y) {
-    // (x, y) 위치에 퀸을 놓을 수 있는지 확인
-    for (let [a, b] of queens) {
-      if (a == x || b == y) return false; // 행이나 열이 같다면 놓을 수 없음
-      if (Math.abs(a - x) == Math.abs(b - y)) return false; // 대각선에 위치한 경우 놓을 수 없음
+  function check(x, y) {
+    for (let i = 0; i < queens.length; i++) {
+      if (x === queens[i][0] || y === queens[i][1]) return false; // 행이나 열이 같다면 놓을 수 없음
+      if (Math.abs(x - queens[i][0]) === Math.abs(y - queens[i][1])) return false; // 대각선에 위치한 경우 놓을 수 없음
     }
     return true;
   }
 
-  function DFS(row) {
-    if (row == n) answer += 1;
-    for (let i = 0; i < n; i++) {
-      if (possible(row, i)) {
-        queens.push([row, i]);
-        DFS(row + 1);
-        queens.pop();
+  function DFS(L) {
+    if (L === n) {
+      answer++;
+      return;
+    } else {
+      for (let i = 0; i < n; i++) {
+        if (check(L, i)) {
+          queens.push([L, i]);
+          DFS(L + 1);
+          queens.pop();
+        }
       }
     }
   }
+
   DFS(0);
   return answer;
 }
