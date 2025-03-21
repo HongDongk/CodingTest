2f04cec628dcb5b467dbd33e13f2a8ec2a9a4bd9
Dongkeun <hongdongk@naver.com>
2025-03-22 00:08:24 +0900

    네트워크다시풀기

--- DFS & BFS/네트워크.js
+++ DFS & BFS/네트워크.js
@@ -4,11 +4,11 @@ function solution(n, computers) {
   let visited = new Array(n).fill(false);
   let answer = 0;
 
-  function dfs(i) {
-    visited[i] = true;
-    for (let j = 0; j < n; j++) {
-      if (visited[j] === false && computers[i][j] === 1) {
-        dfs(j);
+  function dfs(L) {
+    for (let i = 0; i < n; i++) {
+      if (computers[L][i] === 1 && !visited[i]) {
+        visited[i] = true;
+        dfs(i);
       }
     }
   }
