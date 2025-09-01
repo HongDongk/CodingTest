8df95ee69df7ae6c8cc7c47b0c6ed9fda8cbdbd0
dongkeun12 <hongdongk@naver.com>
2025-09-01 15:57:08 +0900

    Update 땅따먹기.js

--- DP/땅따먹기.js
+++ DP/땅따먹기.js
@@ -1,12 +1,28 @@
 function solution(land) {
+  let dp = land[0].slice();
+
   for (let i = 1; i < land.length; i++) {
+    let current = new Array(4).fill(0);
     for (let j = 0; j < 4; j++) {
-      let max = 0;
-      for (let k = 0; k < 4; k++) {
-        if (k === j) continue;
-        max = Math.max(max, land[i - 1][k]);
+      let best = 0;
+      for (let z = 0; z < 4; z++) {
+        if (j === z) continue;
+        if (best < dp[z]) best = dp[z];
       }
-      land[i][j] += max;
+      current[j] = land[i][j] + best;
+    }
+
+    dp = current;
+  }
+
+  return Math.max(...dp);
+}
+
+// filter 사용하기(추천)
+function solution(land) {
+  for (let i = 1; i < land.length; i++) {
+    for (let j = 0; j < land[0].length; j++) {
+      land[i][j] += Math.max(...land[i - 1].filter((a, index) => index !== j));
     }
   }
 
