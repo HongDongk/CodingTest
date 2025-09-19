542d7f3ea4bcb026bc7a0036e88733d61b2eb929
Dongkeun <hongdongk@naver.com>
2025-09-19 16:58:51 +0900

    Update 카카오_징검다리건너기.js

--- 이분탐색/카카오_징검다리건너기.js
+++ 이분탐색/카카오_징검다리건너기.js
@@ -1,15 +1,14 @@
 function solution(stones, k) {
+  let answer;
   let start = 1;
   let end = stones.reduce((a, b) => a + b, 0);
-  let answer = 0;
 
   while (start <= end) {
-    let mid = parseInt((start + end) / 2);
+    let mid = Math.floor((start + end) / 2);
     let count = 0;
-
-    for (let i = 0; i < stones.length; i++) {
+    for (let stone of stones) {
       if (count >= k) break;
-      if (stones[i] - mid <= 0) {
+      if (stone - mid < 0) {
         count++;
       } else {
         count = 0;
@@ -20,8 +19,9 @@ function solution(stones, k) {
       end = mid - 1;
     } else {
       start = mid + 1;
-      answer = Math.max(answer, start);
+      answer = mid;
     }
   }
+
   return answer;
 }
